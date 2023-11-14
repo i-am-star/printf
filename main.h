c270c7a4915c3fc786083ebbf7fc4e5153d50b20
i-am-star <adannaya649@gmail.com>
2023-11-14 07:06:01 +0100

    Holds all function prototypes

--- main.h
+++ main.h
@@ -8,5 +8,12 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_char(va_list val);
+int print_str(va_list val);
+int _strlen(char *str);
+int strlen(const char *str);
+int print_percent(void);
+int print_int(va_list args);
+int print_dec(va_list args);
 
 #endif /* _MAIN_H */
