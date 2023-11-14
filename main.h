b86108e4690b580027e7239801a8803d9261612c
i-am-star <adannaya649@gmail.com>
2023-11-14 06:47:17 +0100

    Holds all function prototypes

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif /* _MAIN_H */
