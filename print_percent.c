b1a151b24af2394a8cf2946765a9025ad001ca07
i-am-star <adannaya649@gmail.com>
2023-11-14 06:52:13 +0100

    A function that prints percentage

---
+++ print_percent.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "main.h"
+#include <time.h>
+/**
+* print_percent - prints the percentage sign
+*
+* Return: 1
+*/
+int print_percent(void)
+{
+	_putchar(37);
+
+	return (1);
+}
