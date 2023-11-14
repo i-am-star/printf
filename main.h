319fc608f605e1df7b1ac466c2ddd919da51c690
i-am-star <adannaya649@gmail.com>
2023-11-14 09:18:16 +0100

    created a new personal directory

--- main.h
+++ main.h
@@ -15,5 +15,7 @@ int strlen(const char *str);
 int print_percent(void);
 int print_int(va_list args);
 int print_dec(va_list args);
+int print_HEX(va_list val);
+int print_HEX_extra(unsigned int num);
 
 #endif /* _MAIN_H */
